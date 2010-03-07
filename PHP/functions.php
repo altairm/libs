638922a79f2bd0a563c4777dadef10d6505158df
AltairM <AltairM@19d71d34-1f82-11de-87de-9d07992f09a1>
2010-03-07 07:53:39 +0000

    useful functions

--- PHP/functions.php
+++ PHP/functions.php
@@ -5,9 +5,9 @@
             $ip = $_SERVER['HTTP_CLIENT_IP'];
         } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
             //to check ip is pass from proxy
-            $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
         } else {
-            $ip=$_SERVER['REMOTE_ADDR'];
+            $ip = $_SERVER['REMOTE_ADDR'];
         }
         return $ip;
     }
